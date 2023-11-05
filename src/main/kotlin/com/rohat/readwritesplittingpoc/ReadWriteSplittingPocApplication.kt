57ea0854a106194e42e525a73973062109b14190
Rohat <rohat347@gmail.com>
2023-11-05 23:30:51 +0300

    change to dark ui theme

--- src/main/kotlin/com/rohat/readwritesplittingpoc/ReadWriteSplittingPocApplication.kt
+++ src/main/kotlin/com/rohat/readwritesplittingpoc/ReadWriteSplittingPocApplication.kt
@@ -1,9 +1,11 @@
 package com.rohat.readwritesplittingpoc
 
 import com.vaadin.flow.component.page.AppShellConfigurator
+import com.vaadin.flow.theme.Theme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@Theme(variant = "dark")
 @SpringBootApplication
 class ReadWriteSplittingPocApplication : AppShellConfigurator
 
